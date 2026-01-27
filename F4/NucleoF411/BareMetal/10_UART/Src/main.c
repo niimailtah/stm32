4a63ba8758d95c30a39800b5e9401c0ae87bfe22
Alexey Polurotov <niimailtah@gmail.com>
2026-01-27 20:15:16 +0500

    Добавлен проект вывода через UART для Nucleo-STM32F411RE

---
+++ F4/NucleoF411/BareMetal/10_UART/Src/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "uart.h"
+
+int main(void)
+{
+	/*Initialize debug UART*/
+	uart_init();
+
+	while (true)
+	{
+		printf("Hello from STM32...\r\n");
+
+	}
+}
